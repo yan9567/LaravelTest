29e6a60f7c8ed12da06a8cf00290df088e7711fc
vagrant <vagrant@homestead>
2021-03-01 16:16:41 +0000

    修改default.blade.php视图，使用bootstrap css样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,23 @@
 <html>
     <head>
         <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+        <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
+
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="/">Weibo App</a>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item"><a class="nav-link" herf="/help">帮助</a></li>
+                <li class="nav-item"><a class="nav-link" herf="#">登录</a></li>
+            </ul>
+        </div>
+      </nav>
+
+      <div class="container">
         @yield('content')
+      </div>
+
     </body>
 </html>
